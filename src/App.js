9330ed9ee920cafc4b213104c8dff2488db61b00
Samrat Jagtap <jagtapsam9@gmail.com>
2023-05-08 21:39:25 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Weather from './components/Weather';
 
+//function to intiate app
 function App() {
   return (
     <div className="App">
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// ReactDOm to render App
 ReactDOM.render(
   <React.StrictMode>
     <App />
